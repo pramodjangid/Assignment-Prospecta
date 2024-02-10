c0b8b4cc560b625231bb7a87718544f1ec40cf88
Dev Srivastav <jangidpramod99@gmail.com>
2024-02-10 18:33:47 +0530

    coding assignment

---
+++ src/main/java/com/prospecta/CodingAssignmentProspectaApplication.java
@@ -0,0 +1,13 @@
+package com.prospecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAssignmentProspectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingAssignmentProspectaApplication.class, args);
+	}
+
+}
